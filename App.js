a55804fc154cb959dc23aa95c8549b80c460d057
AbdurRehman65 <abdur.rehmanf.65@gmail.com>
2023-06-17 13:34:00 +0500

    Added Some Tailwind Packages

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, SafeAreaViewComponent } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
+import { TailwindProvider } from 'tailwindcss-react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Friend! </Text>
-      <StatusBar style="auto" />
-    </View>
+    <TailwindProvider>
+        <SafeAreaView>
+        <Text>hello mensssssssssssssssssssssssssss</Text>
+        <StatusBar style="auto" />
+        </SafeAreaView>
+      
+   </TailwindProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
