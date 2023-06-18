285a13bfdc7e59f07bfecb04246adb9c132b9e25
AbdurRehman65 <abdur.rehmanf.65@gmail.com>
2023-06-18 15:27:45 +0500

    Added components/screen folder with yarn packages

--- App.js
+++ App.js
@@ -2,14 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, SafeAreaViewComponent } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <TailwindProvider>
-        <SafeAreaView>
-        <Text>hello mensssssssssssssssssssssssssss</Text>
-        <StatusBar style="auto" />
-        </SafeAreaView>
+      <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+
+      </NavigationContainer>
+       
       
    </TailwindProvider>
   );
