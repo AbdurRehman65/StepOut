049a1ecae2d9d12ff0631824bbca63ab113178e9
AbdurRehman65 <abdur.rehmanf.65@gmail.com>
2023-06-20 19:02:57 +0500

    Added the Discover.js Screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,6 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
 import React, { useLayoutEffect } from 'react'
+import * as Animatable from 'react-native-animatable';
 import { useNavigation } from '@react-navigation/native'
 
 const HomeScreen = () => {
@@ -14,7 +15,7 @@ const HomeScreen = () => {
   })
   return (
     <SafeAreaView className="flex-1 justify-center align-center">
-    <View className="flex-row">
+    <View className="flex-row absolute top-20 mt-5">
       <View><Text>StepOut</Text></View>
 
     </View>
@@ -23,10 +24,15 @@ const HomeScreen = () => {
       <Text className="text-[#3C6072] text-[32px]">Great Journeys Begin with Simple Steps</Text>
       <Text>Step Out with confidence with your own StepOut buddy</Text>
     </View>
-
-    <View className="absolute bottom-20 w-24 h-24 bg-slate-300 border-l-2 border-r-2 border-t-4 border-[#00BCC9] rounder-full items-center justify-center ">
-      <View className="w-20 h-20 items-center justify-center "><Text>StepIn</Text></View>
-    </View>
+     <TouchableOpacity
+     onPress={ () => navigation.navigate("Discover")} 
+     className="mt-40 pt-40 ml-20">
+    < Animatable.View animation={"pulse"} easing= "ease-in-out" iterationCount= {"infinite"} className="absolute bottom-20 w-24 h-24 border-l-2 border-r-2 border-t-4 items-center justify-center rounded-full bg-[#00BCC9]">
+      <View className="w-20 h-20 items-center justify-center "><Text className="text-gray-50 text-[23px]">StepIn</Text></View>
+      
+    </Animatable.View>
+    </TouchableOpacity>
+   
     </SafeAreaView>
    
   )
