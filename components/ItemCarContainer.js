60f5723f1b0ff9afb41269faddd2726e22bc21be
AbdurRehman65 <abdur.rehmanf.65@gmail.com>
2023-06-22 02:15:36 +0500

    Location Images can be viewed on seperate screens

--- components/ItemCarContainer.js
+++ components/ItemCarContainer.js
@@ -1,10 +1,15 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 import { FontAwesome5 } from '@expo/vector-icons';
 import React from 'react'
+import { useNavigation } from '@react-navigation/native';
 
 const ItemCarContainer = ({imageSrc, title, location, data}) => {
+
+  const navigation = useNavigation();
   return (
-   <TouchableOpacity className="rounded-md border border-gray-300 space-y-2 px-2 py-2 shadow-md bg-white w-[150px] my-2">
+   <TouchableOpacity
+   onPress={() => navigation.navigate("ItemScreen", {param : data})}
+    className="rounded-md border border-gray-300 space-y-2 px-2 py-2 shadow-md bg-white w-[150px] my-2">
        <Image 
        source={{uri : imageSrc}}
        className="w-full h-40 rounded-md object-cover"
--- screens/ItemScreen.js
+++ screens/ItemScreen.js
@@ -1,11 +1,62 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, ScrollView, Image } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { FontAwesome5 } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native'
+import { TouchableOpacity } from 'react-native';
 
-const ItemScreen = () => {
+const ItemScreen = ({route}) => {
+    const navigation = useNavigation();
+
+    const data = route?.params?.param;
+    useLayoutEffect(() => {
+        navigation.setOptions({
+          headerShown: false,
+        })
+    
+      }, []);
+
+      console.log(data);
   return (
-    <View>
-      <Text>ItemScreen</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white relative">
+      <ScrollView className="flex-1 px-4 py-6">
+        <View className="relative bg-white shadow-lg">
+            <Image 
+            source = {
+                {uri: 
+                    data?.photo?.images?.large?.url ?
+                    data?.photo?.images?.large?.url :
+                    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fillustrations%2Ftea-spoon-white-background&psig=AOvVaw1z_cpcAVtVTFQ8NO6r1GpN&ust=1687465223001000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCOiu4PqX1f8CFQAAAAAdAAAAABAE"
+                }
+            }
+            className="w-full h-72 object-cover rounded-2xl"
+            />
+            <View className="absolute flex-row inset-x-0 top-5 justify-between px-6">
+                <TouchableOpacity
+                onPress={() => navigation.navigate("Discover")}
+                 className="w-10 h-10 rounded-md items-center justify-center bg-white">
+                <FontAwesome5 name="chevron-left" size={24} color="black" />
+                </TouchableOpacity>
+                <TouchableOpacity className="w-10 h-10 rounded-md items-center justify-center bg-[#06B2BE]">
+                <FontAwesome5 name="heartbeat" size={24} color="#fff" />
+                </TouchableOpacity>
+
+            </View>
+            <View className="absolute flex-row inset-x-0 top-5 justify-between px-6">
+                <TouchableOpacity
+                onPress={() => navigation.navigate("Discover")}
+                 className="w-10 h-10 rounded-md items-center justify-center bg-white">
+                <FontAwesome5 name="chevron-left" size={24} color="black" />
+                </TouchableOpacity>
+                <TouchableOpacity className="w-10 h-10 rounded-md items-center justify-center bg-[#06B2BE]">
+                <FontAwesome5 name="heartbeat" size={24} color="#fff" />
+                </TouchableOpacity>
+
+            </View>
+
+        </View>
+
+      </ScrollView>
+      </SafeAreaView>
   )
 }
 
